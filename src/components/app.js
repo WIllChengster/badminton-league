acfd4ea13e3bd9dfc9f26c24d30dcd13f14b38a6
Will Cheng <willchengsters@gmail.com>
2018-05-19 21:18:08 -0700

    installed bootstrap , writing basic html mock up

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => (
     <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
+        <nav className="navbar navbar-dark bg-dark ">
+            <h4>SoCal Badminton League </h4>
+        </nav>
+        <div className="container-fluid"></div>
     </div>
 );
 
